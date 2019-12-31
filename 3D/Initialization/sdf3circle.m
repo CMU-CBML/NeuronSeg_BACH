02fb2b022a99f083f804826e37b4c721ec27702f
Aishwarya Pawar <arpawar@andrew.cmu.edu>
2019-12-31 00:16:29 -0500

    Add files via upload

---
+++ 3D/Initialization/sdf3circle.m
@@ -0,0 +1,21 @@
+function f = sdf2circle(nrow,ncol,nslice, ic,jc,kc, r)
+%   sdf2circle(nrow,ncol, ic,jc,r) computes the signed distance to a circle
+%   input: 
+%       nrow: number of rows
+%       ncol: number of columns
+%       (ic,jc): center of the circle
+%       r: radius of the circle
+%   output: 
+%       f: signed distance to the circle
+%  
+%   created on 04/26/2004
+%   author: Chunming Li
+%   email: li_chunming@hotmail.com
+%   Copyright (c) 2004-2006 by Chunming Li
+
+
+[X,Y,Z] = meshgrid(1:ncol, 1:nrow, 1:nslice);
+
+f = sqrt((X-jc).^2 + (Y-ic).^2 + (Z-kc).^2)-r;
+
+%f=sdf2circle(100,50,51,25,10);figure;imagesc(f)
